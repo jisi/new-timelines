786071aed28218bb151eb9d3b7f05528287d60f1
jisi <isa.steiner@siroop.ch>
2016-10-11 02:07:16 +0200

    moved code to the backend

--- src/app/timeline-manager/timeline-manager.component.ts
+++ src/app/timeline-manager/timeline-manager.component.ts
@@ -5,39 +5,78 @@ import {AngularFire, FirebaseListObservable, AngularFireDatabase} from 'angularf
 
 @Pipe({name: 'values', pure: true})
 export class ValuesPipe implements PipeTransform {
-  transform(value:any, args:any[] = null):any {
+  transform(value: any, args: any[] = null): any {
     if (value) {
-      return Object.keys(value).map((key) =>
-      {
+      return Object.keys(value).map((key) => {
         let newObj = value[key];
         newObj['key'] = key;
         return newObj;
-    });
+      });
     }
   }
 }
 
 @Component({
   selector: 'login',
-  templateUrl: './timeline-manager.component.html',
+  template: `
+
+    <form>
+
+      <input type="text" [(ngModel)]="timeline_title" name="userInputTitle">
+<input type="text" [(ngModel)]="timeline_description" name="userInputDescription">
+<button (click)="addTimeline(timeline_title, timeline_description)">Add Timeline</button>
+</form>
+
+<div class="grid-item" *ngFor="let timeline of timelines | async">
+
+<div class="timeline-title">
+  {{timeline.title}} :: {{timeline.description}}
+
+<button class="deleteTimelineButton" type="button" (click)="deleteTimeline(timeline)">Delete Timeline</button>
+</div>
+
+<form>
+
+  <input type="text" [(ngModel)]="timeline.event_date" name="userInputDate + timeline.key" #eventDate="ngModel">
+<input type="text" [(ngModel)]="timeline.event_description" name="userInputEvent" #eventDescription="ngModel">
+
+<button class="addEventButton" type="button" (click)="addEvent(timeline)">Add Event</button>
+
+<div class="event-title">Events:</div>
+
+<div *ngFor="let datapoint of timeline.datapoints | values; let i = index">
+
+<input type="text" [(ngModel)]="datapoint.time" name="datapointTime +{{i}}" >
+<input type="text" [(ngModel)]="datapoint.event" name="datapointEvent +{{i}}" >
+<button class="editEventButton" type="button" (click)="editEvent(timeline, datapoint)">Edit Event</button>
+<button class="deleteEventButton" type="button" (click)="deleteEvent(timeline, datapoint)">Delete Event</button>
+
+</div>
+
+</form>
+
+</div>
+  
+`
+  ,
   styleUrls: ['./timeline-manager.component.scss'],
   providers: [AngularFire]
 })
 
 
 export class TimelineManagerComponent {
-  private timelines:FirebaseListObservable<any[]>;
+  private timelines: FirebaseListObservable<any[]>;
 
   private timeline_title = "";
   private timeline_description = "";
 
-  constructor(private af:AngularFire) {
+  constructor(private af: AngularFire) {
 
     try {
       this.timelines = af.database.list('/timelines');
 
-    }catch(e){
-      console.log('timelines could not be fetched'+e.toString());
+    } catch (e) {
+      console.log('timelines could not be fetched' + e.toString());
     }
 
   }
@@ -52,16 +91,16 @@ export class TimelineManagerComponent {
     });
 
     this.timeline_title = '';
-    this.timeline_description ='';
+    this.timeline_description = '';
 
   }
 
-  private addEvent(timeline:any) {
+  private addEvent(timeline: any) {
 
 
-    console.log("timeline in addEvent: "+ timeline.$key);
+    console.log("timeline in addEvent: " + timeline.$key);
 
-    let localTimeline:any = this.af.database.list('/timelines/' + timeline.$key + '/datapoints');
+    let localTimeline: any = this.af.database.list('/timelines/' + timeline.$key + '/datapoints');
 
     localTimeline.push({
       "time": timeline.event_date,
@@ -70,7 +109,7 @@ export class TimelineManagerComponent {
   }
 
 
-  private editEvent(timeline:any, datapoint:any) {
+  private editEvent(timeline: any, datapoint: any) {
 
     var db = firebase.database();
     var ref = db.ref('/timelines/' + timeline.$key + '/datapoints');
@@ -85,7 +124,7 @@ export class TimelineManagerComponent {
 
   }
 
-  private deleteEvent(timeline:any, datapoint:any) {
+  private deleteEvent(timeline: any, datapoint: any) {
 
     var db = firebase.database();
     var ref = db.ref('/timelines/' + timeline.$key + '/datapoints');
@@ -97,7 +136,7 @@ export class TimelineManagerComponent {
 
   }
 
-  private deleteTimeline(timeline:any) {
+  private deleteTimeline(timeline: any) {
 
     var db = firebase.database();
     var ref = db.ref('/timelines/');
